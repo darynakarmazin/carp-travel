56ac7b212a02513a75bac16e307ca1acd8f5a69b
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-18 11:48:28 +0300

    Added fn to mob menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,10 +1,26 @@
+import Container from "./Container";
+
 interface Props {
   isOpen: boolean;
   onClose: () => void;
 }
 
 function MobileMenu({ isOpen, onClose }: Props) {
-  return <div className={`md:hidden`}></div>;
+  return (
+    <div
+      className={`md:hidden ${
+        isOpen
+          ? "block fixed z-[100] top-0 left-0 w-full h-full bg-[rgba(1, 10, 5, 0.75)] backdrop-blur-xl"
+          : "hidden"
+      }`}
+    >
+      <Container>
+        <button className={`md:hidden`} type="button" onClick={onClose}>
+          CLOSE
+        </button>
+      </Container>
+    </div>
+  );
 }
 
 export default MobileMenu;
