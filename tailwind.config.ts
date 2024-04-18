dec890fb24ffa5501252ae151852df98147d4c59
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-18 08:57:17 +0300

    Updated custom containers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      xl: "1280px",
+    },
   },
   plugins: [],
 };
