4ebef2331e2c68761f561bf0b75aedfdbccb36ca
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-18 16:53:03 +0300

    Added data for About section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,30 @@
 import Container from "../Container";
+import aboutData from "../../data/about.json";
+const {
+  title,
+  descriptionFirst,
+  descriptionSecond,
+  slogan,
+  descriptionBottom,
+} = aboutData;
 
 function About() {
   return (
     <section id="about" className="about-section py-20">
       <Container>
-        <div>About (Who we are)</div>
+        <div>
+          <h2 className={`h2`}>{title}</h2>
+          <p>{descriptionFirst}</p>
+          <p>{descriptionSecond}</p>
+        </div>
+        <div>
+          <div>
+            <p>{slogan[0]}</p>
+            <p>{slogan[1]}</p>
+            <p>{slogan[2]}</p>
+          </div>
+          <p>{descriptionBottom}</p>
+        </div>
       </Container>
     </section>
   );
--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -8,7 +8,7 @@ interface Props {
 function MobileMenu({ isOpen, onClose }: Props) {
   return (
     <div
-      className={`md:hidden block fixed z-[100] top-0 left-0 w-full h-full bg-[rgba(1, 10, 5, 0.75)] backdrop-blur-xl transition-all duration-500 ease-in-out ${
+      className={`md:hidden block fixed z-[100] top-0 left-0 w-full h-full bg-[#010A05BF]/[0.75] backdrop-blur-xl transition-all duration-500 ease-in-out ${
         isOpen ? "opacity-100 visible" : "opacity-0 invisible"
       }`}
     >
