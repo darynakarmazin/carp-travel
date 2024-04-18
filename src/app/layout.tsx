b28bcc1e12610bbc03a48e5bee4a4e9d5b4db344
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-18 10:10:51 +0300

    Updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="./img/icon.svg" type="image/svg" sizes="16x16" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
