a911013fd65e6a315cd7628d6be77a875600a0ac
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-18 14:55:45 +0300

    Changed adaptive for mob

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,21 +5,24 @@ const { title, cities, slogan, description, buttonText } = heroData;
 
 function Hero() {
   return (
-    <section id="hero" className={`hero-section pt-[130px] pb-[104px] `}>
+    <section
+      id="hero"
+      className={`hero-section pt-[105px] pb-[56px] md:pt-[121px] md:pb-[64px] xl:pt-[130px] xl:pb-[104px]`}
+    >
       <Container>
         <div className={`flex flex-col md:flex-row justify-between`}>
           <div
-            className={`md:hidden w-[320px] mb-[181px] text-[98px] leading-tight font-medium`}
+            className={`md:hidden ml-auto mb-6 text-[40px] leading-none font-medium`}
           >
             <p>
               {slogan[0]}&nbsp;
               <span className={`font-thin`}>{slogan[1]}</span>
             </p>
-            <p className={`text-base tracking-[2.5em] font-light`}>
+            <p className={`text-base tracking-[0.6em] font-light`}>
               {slogan[2]}
             </p>
           </div>
-          <div className={`w-1/2`}>
+          <div className={`md:w-1/2`}>
             <h1
               className={`text-[40px] md:text-[67px] xl:text-[98px] uppercase font-thin tracking-tighter leading-[56px] md:leading-[81px] xl:leading-[119px] xl:pt-6 xl:pl-6 mb-6 md:mb-[64px] xl:mb-[148px]`}
             >
@@ -27,14 +30,14 @@ function Hero() {
               {title[2]}
             </h1>
             <p
-              className={`text-base font-extralight leading-normal tracking-wider xl:pl-6 xl:mb-6`}
+              className={`max-md:w-[175px] text-[10px] leading-relaxed xl:text-base font-extralight xl:leading-normal tracking-wider xl:pl-6 mb-6`}
             >
               {cities}
             </p>
           </div>
           <div className={`flex flex-col items-end`}>
             <div
-              className={`max-md:hidden w-[320px] mb-[181px] text-[98px] leading-tight font-medium`}
+              className={`max-md:hidden xl:w-[320px] mb-[181px] text-[98px] leading-tight font-medium`}
             >
               <p>
                 {slogan[0]}&nbsp;
@@ -44,8 +47,10 @@ function Hero() {
                 {slogan[2]}
               </p>
             </div>
-            <div className={`w-[294px]`}>
-              <p className={`text-[18px] leading-6 font-extralight mb-[28px]`}>
+            <div className={`w-full md:w-[230px] xl:w-[294px]`}>
+              <p
+                className={`text-justify text-sm leading-5 mb-6 md:mb-[28px] md:text-base xl:text-[18px] xl:leading-6 font-extralight`}
+              >
                 {description}
               </p>
               <JoinButton buttonText={buttonText} />
