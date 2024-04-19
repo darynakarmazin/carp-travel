f36cea58e5bb07ecf73fd26c5b5e966545e83a36
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-19 10:17:21 +0300

    Added services data json

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,18 +1,50 @@
 "use client";
 
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+
 import Container from "../Container";
+
 import servicesData from "../../data/services.json";
-const { title } = servicesData;
+const { title, slides } = servicesData;
 
 function Services() {
+  const [activeSlide, setActiveSlide] = useState<number>(0);
+  const swiperRef = useRef<any>(null);
+
+  const handleLinkClick = (idx: number): void => {
+    setActiveSlide(idx);
+
+    if (swiperRef.current && swiperRef.current.slideTo) {
+      swiperRef.current.slideTo(idx);
+    }
+  };
+
   return (
     <section id="services" className="services-section py-20">
-      <Container>
-        <h2 className="text-[40px] md:text-[67px] xl:text-[98px] leading-snug md:leading-tight font-medium tracking-[-0.064em] mb-2">
-          <span className="font-thin">{title[0]}</span>
-          {title[1]}
-        </h2>
-      </Container>
+      <Swiper
+        modules={[EffectFade, A11y]}
+        slidesPerView={1}
+        initialSlide={0}
+        effect={"fade"}
+        onSlideChange={(swiper) => {
+          setActiveSlide(swiper.realIndex);
+        }}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+      >
+        <Container>
+          <h2 className="text-[40px] md:text-[67px] xl:text-[98px] leading-snug md:leading-tight font-medium tracking-[-0.064em] mb-2">
+            <span className="font-thin">{title[0]}</span>
+            {title[1]}
+          </h2>
+        </Container>
+      </Swiper>
     </section>
   );
 }
