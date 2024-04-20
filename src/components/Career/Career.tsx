6bbece288d5f6338070c5f13a424c345b5d957d0
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-20 21:58:41 +0300

    Added responsiveness for tab+deck "why us?" form

--- src/components/Career/Career.tsx
+++ src/components/Career/Career.tsx
@@ -8,41 +8,40 @@ function Career() {
     <>
       <section id="career" className="career-section py-20">
         <Container>
-          <div>
-            <div className="mb-8 md:flex justify-between md:mb-[5px] xl:mb-[31px]">
-              <h2 className="text-[40px] md:text-[67px] xl:text-[98px] leading-snug md:leading-tight font-medium tracking-[-0.064em] max-md:mb-6">
-                <span className="font-thin">{title[0]}</span>
-                {title[1]}
-              </h2>
-              <p
-                className={`max-md:ml-[101px] md:text-justify md:max-w-[221px] xl:max-w-[293px] text-sm leading-5 max-md:mb-6 md:mt-[10px] md:text-[13px] xl:text-[18px] xl:leading-6 font-extralight`}
-              >
-                {description}
-              </p>
-            </div>
-            <div>
-              <h3 className="max-md:ml-[100px] text-3xl xl:text-4xl font-extralight mb-9 md:mb-14 xl:mb-11 uppercase md:ml-[80px] xl:ml-[147px]">
-                {whyList[0].title}
-              </h3>
-              <ul className="max-md:mr-[100px] text-right">
-                {whyList[0].list.map((item) => {
-                  return (
-                    <li className="xl:flex xl:gap-6" key={item.qv}>
-                      <p className="text-sm md:text-[16px] xl:text-[18px] leading-5  xl:leading-6 font-normal mb-2 xl:mb-0">
-                        {item.qv}
-                      </p>
-                      <p className="text-[12px] leading-5 font-extralight mb-4 md:mb-6">
-                        {item.ans}
-                      </p>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+          <div className="mb-8 md:flex justify-between md:mb-[5px] xl:mb-[31px]">
+            <h2 className="text-[40px] md:text-[67px] xl:text-[98px] leading-snug md:leading-tight font-medium tracking-[-0.064em] max-md:mb-6">
+              <span className="font-thin">{title[0]}</span>
+              {title[1]}
+            </h2>
+            <p
+              className={`max-md:ml-[101px] md:text-justify md:max-w-[221px] xl:max-w-[293px] text-sm leading-5 max-md:mb-6 md:mt-[10px] md:text-[13px] xl:text-[18px] xl:leading-6 font-extralight`}
+            >
+              {description}
+            </p>
           </div>
-          <div className="max-md:hidden">
-            <p>{phrase}</p>
-            <CareerForm />
+
+          <h3 className="max-md:ml-[100px] text-3xl xl:text-4xl font-extralight mb-9 md:mb-14 xl:mb-11 uppercase md:ml-[80px] xl:ml-[147px]">
+            {whyList[0].title}
+          </h3>
+          <div className="md:flex md:gap-5 xl:gap-6">
+            <ul className="max-md:mr-[100px] text-right md:w-1/3 xl:w-1/2">
+              {whyList[0].list.map((item) => {
+                return (
+                  <li className="xl:flex xl:gap-6 xl:justify-end" key={item.qv}>
+                    <p className="text-sm md:text-[16px] xl:text-[18px] leading-5  xl:leading-6 font-normal mb-2 xl:mb-0">
+                      {item.qv}
+                    </p>
+                    <p className="text-[12px] leading-5 font-extralight mb-4 md:mb-6 xl:w-[285px] xl:text-start">
+                      {item.ans}
+                    </p>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="max-md:hidden md:w-2/3 xl:w-1/2">
+              <p>{phrase}</p>
+              <CareerForm />
+            </div>
           </div>
         </Container>
       </section>
