4cf52745759dad163c5339d268a8d24f8ddcf180
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-20 17:47:57 +0300

    Updated og img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     siteName: "CarpTravel",
     images: [
       {
-        url: "/og-image.png",
+        url: "https://github.com/darynakarmazin/carp-travel/raw/main/src/app/og-image.png",
         width: 1200,
         height: 630,
         alt: "CarpTravel",
