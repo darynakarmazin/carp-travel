215268cab840e1104be48ed8cd9c3a56bea88895
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-20 21:45:52 +0300

    Added styles for Career section fonts

--- src/components/Career/Career.tsx
+++ src/components/Career/Career.tsx
@@ -5,36 +5,56 @@ const { title, description, whyList, phrase } = careerData;
 
 function Career() {
   return (
-    <section id="career" className="career-section py-20">
-      <Container>
-        <div>
+    <>
+      <section id="career" className="career-section py-20">
+        <Container>
           <div>
-            <h2 className="text-[40px] md:text-[67px] xl:text-[98px] leading-snug md:leading-tight font-medium tracking-[-0.064em] mb-2">
-              <span className="font-thin">{title[0]}</span>
-              {title[1]}
-            </h2>
-            <p>{description}</p>
+            <div className="mb-8 md:flex justify-between md:mb-[5px] xl:mb-[31px]">
+              <h2 className="text-[40px] md:text-[67px] xl:text-[98px] leading-snug md:leading-tight font-medium tracking-[-0.064em] max-md:mb-6">
+                <span className="font-thin">{title[0]}</span>
+                {title[1]}
+              </h2>
+              <p
+                className={`max-md:ml-[101px] md:text-justify md:max-w-[221px] xl:max-w-[293px] text-sm leading-5 max-md:mb-6 md:mt-[10px] md:text-[13px] xl:text-[18px] xl:leading-6 font-extralight`}
+              >
+                {description}
+              </p>
+            </div>
+            <div>
+              <h3 className="max-md:ml-[100px] text-3xl xl:text-4xl font-extralight mb-9 md:mb-14 xl:mb-11 uppercase md:ml-[80px] xl:ml-[147px]">
+                {whyList[0].title}
+              </h3>
+              <ul className="max-md:mr-[100px] text-right">
+                {whyList[0].list.map((item) => {
+                  return (
+                    <li className="xl:flex xl:gap-6" key={item.qv}>
+                      <p className="text-sm md:text-[16px] xl:text-[18px] leading-5  xl:leading-6 font-normal mb-2 xl:mb-0">
+                        {item.qv}
+                      </p>
+                      <p className="text-[12px] leading-5 font-extralight mb-4 md:mb-6">
+                        {item.ans}
+                      </p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
+          <div className="max-md:hidden">
+            <p>{phrase}</p>
+            <CareerForm />
+          </div>
+        </Container>
+      </section>
+      <section className="md:hidden career-section py-20">
+        <Container>
           <div>
-            <h3>{whyList[0].title}</h3>
-            <ul>
-              {whyList[0].list.map((item) => {
-                return (
-                  <li key={item.qv}>
-                    <p>{item.qv}</p>
-                    <p>{item.ans}</p>
-                  </li>
-                );
-              })}
-            </ul>
+            <p>{phrase}</p>
+            <CareerForm />
           </div>
-        </div>
-        <div>
-          <p>{phrase}</p>
-          <CareerForm />
-        </div>
-      </Container>
-    </section>
+        </Container>
+      </section>
+    </>
   );
 }
 
--- src/components/CareerForm.tsx
+++ src/components/CareerForm.tsx
@@ -12,32 +12,93 @@ type Inputs = {
 };
 
 function CareerForm() {
-  const { register, handleSubmit } = useForm<Inputs>();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      phone: "",
+      message: "",
+      agreement: false,
+    },
+  });
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch());
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>
         Full name
-        <input {...register("fullName")} placeholder="Full Name" />
+        <input
+          {...register("fullName", {
+            required: "Full name is required",
+            minLength: {
+              value: 2,
+              message: "Incorrect name",
+            },
+            maxLength: {
+              value: 20,
+              message: "Incorrect name",
+            },
+          })}
+          placeholder="John Smith"
+        />
+        {errors.fullName && <p>{errors.fullName?.message}</p>}
       </label>
       <label>
         E-mail
-        <input {...register("email")} placeholder="E-mail" />
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Incorrect email",
+            },
+          })}
+          placeholder="johnsmith@email.com"
+        />
+        {errors.email && <p>{errors.email?.message}</p>}
       </label>
       <label>
         Position
-        <input {...register("position")} placeholder="Position" />
+        <input
+          {...register("position", {
+            required: "Position is required",
+          })}
+          placeholder="Movie maker"
+        />
+        {errors.position && <p>{errors.position?.message}</p>}
       </label>
       <label>
         Phone
-        <input {...register("phone")} placeholder="Phone" />
+        <input
+          {...register("phone", {
+            required: "Phone is required",
+          })}
+          placeholder="+ 38 (097) 12 34 567"
+        />
+        {errors.phone && <p>{errors.phone?.message}</p>}
       </label>
       <label>
         Message
-        <textarea {...register("message")} placeholder="Message" />
+        <textarea
+          {...register("message", {
+            required: "Message is required",
+          })}
+        />
+        {errors.message && <p>{errors.message?.message}</p>}
       </label>
-      <input {...register("agreement")} type="checkbox" />
+      <label>
+        <input {...register("agreement", { required: true })} type="checkbox" />
+        I confirm my consent to the processing of personal data.
+      </label>
+      {errors.agreement && <p>You must agree to the terms and conditions</p>}
+
       <input type="submit" value="SEND" />
     </form>
   );
