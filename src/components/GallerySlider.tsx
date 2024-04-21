e6492f15a9de22fca8ff67afd2b49821a0ae6bfe
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-21 21:35:46 +0300

    Added GallerySlider.tsx

--- src/components/GallerySlider.tsx
+++ src/components/GallerySlider.tsx
@@ -1,5 +1,91 @@
+"use client";
+
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+import galleryData from "../data/gallery.json";
+const { slides } = galleryData;
+
 function GallerySlider() {
-  return <div className="max-md:hidden">GallerySlider</div>;
+  return (
+    <div className="max-md:hidden">
+      <Swiper
+        navigation={{
+          nextEl: ".custom_next",
+          prevEl: ".custom_prev",
+        }}
+        modules={[Navigation]}
+        slidesPerView={3}
+        loop
+        centeredSlides
+        breakpoints={{
+          0: {
+            enabled: false,
+            slidesPerView: -1,
+          },
+          768: {
+            enabled: true,
+            spaceBetween: 146,
+            width: 703,
+            height: 294,
+            creativeEffect: {
+              limitProgress: 1,
+              prev: {
+                scale: 0.29,
+                translate: ["-69.3%", 0, 0],
+              },
+              next: {
+                scale: 0.29,
+                translate: ["69.3%", 0, 0],
+              },
+            },
+          },
+          1440: {
+            spaceBetween: 650,
+            width: 1280,
+            height: 450,
+            creativeEffect: {
+              limitProgress: 1,
+              prev: {
+                scale: 0.52,
+                translate: ["-76%", 0, 0],
+              },
+              next: {
+                scale: 0.52,
+                translate: ["76%", 0, 0],
+              },
+            },
+          },
+        }}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <Image
+              src={slide.image}
+              alt="View of nature"
+              width={121}
+              height={86}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div>
+        <button
+          type="button"
+          className="custom_prev"
+          aria-label="Previous slide"
+        >
+          BACK
+        </button>
+        <button type="button" className="custom_next" aria-label="Next slide">
+          NEXT
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default GallerySlider;
