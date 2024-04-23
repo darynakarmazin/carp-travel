b7601cf4caecd612efb72331de9d7bea25525f9f
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-23 12:59:34 +0300

    Updated Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 function Container({ children }: ContainerProps) {
   return (
-    <div className="container mx-auto min-w-[320px] px-5 sm:w-[480px] md:w-[768px] md:px-8 xl:px-20 xl:min-w-[1280px] xl:w-full xl:max-w-[1440px] border border-sky-500">
+    <div className="container mx-auto min-w-[320px] px-5 sm:w-[480px] md:w-[768px] md:px-8 xl:px-20 xl:min-w-[1280px] xl:w-full xl:max-w-[1440px]">
       {children}
     </div>
   );
