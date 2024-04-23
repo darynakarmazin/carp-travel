466eba2a4f2c1046f95cd8291a8431489d4b7775
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-23 09:40:07 +0300

    Added Contacts section and responsive bg

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,10 +1,15 @@
 import Container from "../Container";
+import contactsData from "../../data/contacts.json";
+const { title } = contactsData;
 
 function Contacts() {
   return (
-    <section id="contacts" className="py-20">
+    <section id="contacts" className="contacts-section py-20">
       <Container>
-        <div>Contacts (Contact us)</div>
+        <h2 className="text-[40px] md:text-[67px] xl:text-[98px] leading-snug md:leading-tight font-medium tracking-[-0.064em] max-md:mb-6">
+          <span className="font-thin">{title[0]}</span>
+          {title[1]}
+        </h2>
       </Container>
     </section>
   );
